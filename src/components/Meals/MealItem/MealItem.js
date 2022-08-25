53b78aa60775f5ece9c49e70e3f9c446213ec7e8
satrio priambodo <satriopriambodo96@gmail.com>
2022-08-25 11:10:13 +0700

    food order

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -16,17 +16,14 @@ const MealItem = (props) => {
     });
   };
   return (
-    <li>
+    <li className={classes.meal}>
       <div>
-        <div className={classes.meal}>
-          <h3>{props.name}</h3>
-          <div className={classes.description}>{props.description}</div>
-          <div className={classes.price}>{price}</div>
-        </div>
-
-        <div>
-          <MealItemForm onAddToCart={addToCartHandler} />
-        </div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
